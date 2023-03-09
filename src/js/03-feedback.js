930c8afb3fe9448ff44f67059bb0da8acf40f66a
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-03-09 21:43:05 +0100

    HW 08 v3.0

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,31 +5,27 @@ const email = document.querySelector('input');
 const message = document.querySelector('textarea');
 
 regainInputs();
+
 form.addEventListener(
   'input',
-  throttle(e => {
-    e.preventDefault();
-    const INPUTS = { email: email.value, message: message.value };
-    const setInputsJSON = JSON.stringify(INPUTS);
+  throttle(() => {
+    const inputs = { email: email.value, message: message.value };
+    const setInputsJSON = JSON.stringify(inputs);
     localStorage.setItem('feedback-form-state', setInputsJSON);
   }, 500)
-);
+  );
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  onsubmit = e => {
+  form.addEventListener('submit', e => {
     e.preventDefault();
-    const INPUTS = { email: email.value, message: message.value };
-    console.log(INPUTS);
-    form.reset();
     localStorage.clear();
-  };
-});
-
-function regainInputs() {
+    console.log({ email: email.value, message: message.value });
+    form.reset();
+  });
+  
+  function regainInputs() {
   const getInputsJSON = localStorage.getItem('feedback-form-state');
+  const outputsJSON = JSON.parse(getInputsJSON);
   if (getInputsJSON !== null) {
-    const outputsJSON = JSON.parse(getInputsJSON);
     email.value = outputsJSON.email || '';
     message.value = outputsJSON.message || '';
   }
